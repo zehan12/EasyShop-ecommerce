48c7f1a52bbce07f0848365cf31cd21f21af3e8f
Zehan Khan <zkhan@hootboard.com>
2024-04-18 15:06:35 +0530

    update app name

--- backend/src/main/java/com/ecommerce/Application.java
+++ backend/src/main/java/com/ecommerce/Application.java
@@ -1,13 +1,13 @@
-package com.ecommerce.backend;
+package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
