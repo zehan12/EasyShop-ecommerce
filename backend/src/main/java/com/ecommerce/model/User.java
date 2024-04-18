0d99d1a435960c994e19dcb38b4f183a65108420
Zehan Khan <zkhan@hootboard.com>
2024-04-18 19:45:51 +0530

    update update user model with lombok to avoid using multiple getter setter

--- backend/src/main/java/com/ecommerce/model/User.java
+++ backend/src/main/java/com/ecommerce/model/User.java
@@ -9,12 +9,22 @@ import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -53,9 +63,5 @@ public class User {
     private List<Review> reviews = new ArrayList<>();
 
     private LocalDateTime createdAt;
-
-    public User() {
-
-    }
 
 }
