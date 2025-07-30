99dcf61838a1ebd99bb266605270a5158cf6b2a7
zehan12 <zehan9211@gmail.com>
2025-07-31 04:21:22 +0530

    remove backend from java migrate to next js

--- backend/src/main/java/com/ecommerce/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByEmail(String email);
-}
